c168f4fe9c80e324e37d7c017ee1098cc7611dfb
Joakim Nyman <joakim.nyman86@gmail.com>
2020-11-28 11:54:57 +0100

    copy wallet password to /var

--- install_prysm.sh
+++ install_prysm.sh
@@ -18,6 +18,11 @@ sudo chown -R prysm-validator:prysm-validator /var/lib/prysm/validator
 # Import account
 sudo ./prysm.sh validator accounts import --keys-dir=./validator_keys --accept-terms-of-use --pyrmont --wallet-password-file ./validator_keys/wallet_password.txt --wallet-dir /var/lib/prysm/validator --account-password-file ./validator_keys/account_password.txt
 
+# Copy wallet password file for validator to read
+sudo cp ./validator_keys/wallet_password.txt /var/lib/prysm/validator/wallet_password.txt
+sudo chown prysm-validator:prysm-validator /var/lib/prysm/validator/wallet_password.txt
+sudo chmod go-rw /var/lib/prysm/validator/wallet_password.txt
+
 # Copy binaries to bin
 sudo cp dist/beacon*amd64 /usr/local/bin/beacon-chain
 sudo cp dist/validator*amd64 /usr/local/bin/validator
