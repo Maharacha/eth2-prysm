cdab64bd7a1ea90cb098d2ec86862ffdfd3a1e22
Joakim Nyman <joakim.nyman86@gmail.com>
2020-11-28 22:43:08 +0100

    updated against the guide

--- install_prysm.sh
+++ install_prysm.sh
@@ -6,21 +6,21 @@ curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --out
 ./prysm.sh validator --download-only
 
 # Create beacon user and directory
-sudo useradd --no-create-home --shell /bin/false prysm-beaconchain
-sudo mkdir -p /var/lib/prysm/beaconchain
-sudo chown -R prysm-beaconchain:prysm-beaconchain /var/lib/prysm/beaconchain
+sudo useradd --no-create-home --shell /bin/false prysmbeacon
+sudo mkdir -p /var/lib/prysm/beacon
+sudo chown -R prysmbeacon:prysmbeacon /var/lib/prysm/beacon
 
 # Create validator user and directory
-sudo useradd --no-create-home --shell /bin/false prysm-validator
+sudo useradd --no-create-home --shell /bin/false prysmvalidator
 sudo mkdir -p /var/lib/prysm/validator
-sudo chown -R prysm-validator:prysm-validator /var/lib/prysm/validator
+sudo chown -R prysmvalidator:prysmvalidator /var/lib/prysm/validator
 
 # Import account
 sudo ./prysm.sh validator accounts import --keys-dir=./validator_keys --accept-terms-of-use --pyrmont --wallet-password-file ./validator_keys/wallet_password.txt --wallet-dir /var/lib/prysm/validator --account-password-file ./validator_keys/account_password.txt
 
 # Copy wallet password file for validator to read
 sudo cp ./validator_keys/wallet_password.txt /var/lib/prysm/validator/wallet_password.txt
-sudo chown prysm-validator:prysm-validator /var/lib/prysm/validator/wallet_password.txt
+sudo chown prysmvalidator:prysmvalidator /var/lib/prysm/validator/wallet_password.txt
 sudo chmod go-rw /var/lib/prysm/validator/wallet_password.txt
 
 # Copy binaries to bin
@@ -30,7 +30,7 @@ sudo cp dist/validator*amd64 /usr/local/bin/validator
 # Create(copy) and start services
 sudo cp *.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start prysm-beaconchain
-sudo systemctl start prysm-validator
-sudo systemctl enable prysm-beaconchain
-sudo systemctl enable prysm-validator
+sudo systemctl start prysmbeacon
+sudo systemctl start prysmvalidator
+sudo systemctl enable prysmbeacon
+sudo systemctl enable prysmvalidator
