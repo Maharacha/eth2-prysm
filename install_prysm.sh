a5b26418e9d499344d9b67551ec78dd9130938c8
Joakim Nyman <joakim.nyman86@gmail.com>
2020-11-28 11:34:24 +0100

    fixed correct password files and copying binaries

--- install_prysm.sh
+++ install_prysm.sh
@@ -16,10 +16,11 @@ sudo mkdir -p /var/lib/prysm/validator
 sudo chown -R prysm-validator:prysm-validator /var/lib/prysm/validator
 
 # Import account
-sudo ./prysm.sh validator accounts import --keys-dir=./validator_keys --accept-terms-of-use --pyrmont --wallet-password-file ./validator_keys/password.txt --wallet-dir /var/lib/prysm/validator --account-password-file ./validator_keys/password.txt
+sudo ./prysm.sh validator accounts import --keys-dir=./validator_keys --accept-terms-of-use --pyrmont --wallet-password-file ./validator_keys/wallet_password.txt --wallet-dir /var/lib/prysm/validator --account-password-file ./validator_keys/account_password.txt
 
 # Copy binaries to bin
-sudo cp dist/*amd64 /usr/local/bin
+sudo cp dist/beacon*amd64 /usr/local/bin/beacon-chain
+sudo cp dist/validator*amd64 /usr/local/bin/validator
 
 # Create(copy) and start services
 sudo cp *.service /etc/systemd/system/
