65d2f32d9f440514d350b899fb278ade7a6e5b47
Joakim Nyman <joakim.nyman86@gmail.com>
2020-11-28 11:20:08 +0100

    scripts and services added

---
+++ install_prysm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Download beacon and validator binaries using prysm script
+curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output ./prysm.sh && chmod +x ./prysm.sh
+./prysm.sh beacon-chain --download-only
+./prysm.sh validator --download-only
+
+# Create beacon user and directory
+sudo useradd --no-create-home --shell /bin/false prysm-beaconchain
+sudo mkdir -p /var/lib/prysm/beaconchain
+sudo chown -R prysm-beaconchain:prysm-beaconchain /var/lib/prysm/beaconchain
+
+# Create validator user and directory
+sudo useradd --no-create-home --shell /bin/false prysm-validator
+sudo mkdir -p /var/lib/prysm/validator
+sudo chown -R prysm-validator:prysm-validator /var/lib/prysm/validator
+
+# Import account
+sudo ./prysm.sh validator accounts import --keys-dir=./validator_keys --accept-terms-of-use --pyrmont --wallet-password-file ./validator_keys/password.txt --wallet-dir /var/lib/prysm/validator --account-password-file ./validator_keys/password.txt
+
+# Copy binaries to bin
+sudo cp dist/*amd64 /usr/local/bin
+
+# Create(copy) and start services
+sudo cp *.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start prysm-beaconchain
+sudo systemctl start prysm-validator
+sudo systemctl enable prysm-beaconchain
+sudo systemctl enable prysm-validator
